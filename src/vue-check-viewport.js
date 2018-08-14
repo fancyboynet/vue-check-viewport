d1e5948cf184c0773784ca85e1b60ed8268a2728
Fancy <fancyboynet@gmail.com>
2018-08-14 09:16:57 +0800

    增加语法检查

--- src/vue-check-viewport.js
+++ src/vue-check-viewport.js
@@ -1,10 +1,10 @@
 let observer = null
 function createObserver (options) {
-  observer = new IntersectionObserver((entries) => {
+  observer = new window.IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      if(entry.intersectionRatio < options.intersectionRatio){
+      if (entry.intersectionRatio < options.intersectionRatio) {
         entry.target.__vue__.onUnVisible()
-      }else{
+      } else {
         entry.target.__vue__.onVisible()
       }
     })
@@ -17,28 +17,28 @@ function isElementInViewport (el, options) {
   let vH = window.innerHeight || document.documentElement.clientHeight
   let intersectionW = rect.width
   let intersectionH = rect.height
-  if(rect.bottom <= 0){
+  if (rect.bottom <= 0) {
     return false
   }
-  if(rect.top >= vH){
+  if (rect.top >= vH) {
     return false
   }
-  if(rect.right <= 0){
+  if (rect.right <= 0) {
     return false
   }
-  if(rect.left >= vW){
+  if (rect.left >= vW) {
     return false
   }
-  if(rect.top < 0){
+  if (rect.top < 0) {
     intersectionH = intersectionH + rect.top
   }
-  if(rect.bottom > vH){
+  if (rect.bottom > vH) {
     intersectionH = intersectionH + vH - rect.bottom
   }
-  if(rect.left < 0){
+  if (rect.left < 0) {
     intersectionW = intersectionW + rect.left
   }
-  if(rect.right > vW){
+  if (rect.right > vW) {
     intersectionW = intersectionW + vW - rect.right
   }
   return (intersectionW * intersectionH) >= (rect.width * rect.height * options.intersectionRatio)
@@ -53,7 +53,7 @@ export default {
     },
     options: {
       type: Object,
-      default(){
+      default () {
         return {
           root: null,
           rootMargin: '0px',
@@ -63,81 +63,81 @@ export default {
       }
     }
   },
-  data() {
+  data () {
     return {
       isVisible: undefined
     }
   },
-  render(createElement) {
+  render (createElement) {
     return createElement(
       this.tag,
       this.$slots.default
     )
   },
-  mounted(){
+  mounted () {
     this.initObserver()
   },
-  beforeDestroy(){
+  beforeDestroy () {
     this.destroyObserver()
   },
   methods: {
-    initObserver(){
-      if(window.IntersectionObserver){
+    initObserver () {
+      if (window.IntersectionObserver) {
         this.initIntersectionObserver()
         return
       }
       this.initFallback()
     },
-    initIntersectionObserver(){
-      if(!observer){
+    initIntersectionObserver () {
+      if (!observer) {
         createObserver(this.options)
       }
       observer.observe(this.$el)
     },
-    initFallback(){
+    initFallback () {
       let root = this.options.root || window
       root.addEventListener('scroll', this.listener, false)
       window.addEventListener('load', this.listener, false)
       window.addEventListener('resize', this.listener, false)
       window.addEventListener('DOMContentLoaded', this.listener, false)
     },
-    listener(){
-      if(isElementInViewport(this.$el, this.options)){
+    listener () {
+      if (isElementInViewport(this.$el, this.options)) {
         this.onVisible()
         return
       }
       this.onUnVisible()
     },
-    onVisible(){
-      if (this.isVisible === true){
+    onVisible () {
+      if (this.isVisible === true) {
         return
       }
       this.isVisible = true
       this.$emit('on-visible')
     },
-    onUnVisible(){
-      if (this.isVisible === false){
+    onUnVisible () {
+      if (this.isVisible === false) {
         return
       }
       this.isVisible = false
       this.$emit('on-un-visible')
     },
-    destroyObserver(){
-      if(window.IntersectionObserver){
+    destroyObserver () {
+      if (window.IntersectionObserver) {
         this.destroyIntersectionObserver()
         return
       }
       this.destroyFallback()
     },
-    destroyFallback(){
+    destroyFallback () {
       let root = this.options.root || window
       root.removeEventListener('scroll', this.listener, false)
       window.removeEventListener('load', this.listener, false)
       window.removeEventListener('resize', this.listener, false)
       window.removeEventListener('DOMContentLoaded', this.listener, false)
     },
-    destroyIntersectionObserver(){
-      if(!observer){
+    destroyIntersectionObserver () {
+      if (!observer) {
         return
       }
       observer.unobserve(this.$el)
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path')
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const pkgJson = require('./package')
 const basic = {
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    chunkFilename: "[name].min.js",
-    libraryTarget: "umd",
-    library: "vue-check-viewport"
+    chunkFilename: '[name].min.js',
+    libraryTarget: 'umd',
+    library: 'vue-check-viewport'
   },
   resolve: {
     alias: {
@@ -19,45 +21,49 @@ const basic = {
     rules: [
       {
         test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              '@babel/plugin-transform-runtime'
-            ]
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-transform-runtime'
+              ]
+            }
+          },
+          {
+            loader: 'eslint-loader'
           }
-        },
+        ],
         exclude: /node_modules/
       }
     ]
   }
 }
 
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
   module.exports = merge(basic, {
     mode: 'development',
     entry: {
-      'demo': "./demo/index.js"
+      'demo': './demo/index.js'
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "demo/index.html")
+        template: path.resolve(__dirname, 'demo/index.html')
       })
     ],
     devtool: 'inline-source-map',
-    devServer: {
-
-    }
+    devServer: {}
   })
-}else{
+} else {
   module.exports = merge(basic, {
     mode: 'production',
     entry: {
-      'vue-check-viewport': "./src/vue-check-viewport.js"
+      'vue-check-viewport': './src/vue-check-viewport.js'
     },
     plugins: [
-      new CleanWebpackPlugin()
+      new CleanWebpackPlugin(),
+      new webpack.BannerPlugin(`vue-check-viewport v${pkgJson.version}`)
     ]
   })
 }
