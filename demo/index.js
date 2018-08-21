1aaa482daeea2b392bbe7530666fb2b7aaaeadf6
Fancy <fancyboynet@gmail.com>
2018-08-21 10:45:31 +0800

    add some handler in demo

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueCheckViewport from '../src/vue-check-viewport'
 function createList () {
   let list = []
-  for(let i = 0; i < 20; i++){
+  for (let i = 0; i < 1; i++) {
     list.push({
       val: i,
       visited: false
@@ -10,22 +10,31 @@ function createList () {
   }
   return list
 }
-new Vue({
-  el: '#app',
+let vm = new Vue({
   data: {
     list: createList()
   },
   methods: {
-    onVisible(index){
+    onVisible (index) {
       console.log('in', index)
       this.$set(this.list[index], 'visited', true)
     },
-    onUnVisible(index){
+    onUnVisible (index) {
       console.log('out', index)
       this.$set(this.list[index], 'visited', false)
+    },
+    removeTopOne () {
+      this.list.shift()
+    },
+    reRenderTopOne () {
+      this.$refs.check[0].$forceUpdate()
     }
   },
   components: {
     VueCheckViewport
+  },
+  mounted () {
+    console.log(this.$refs.check)
   }
 })
+vm.$mount('#app')
